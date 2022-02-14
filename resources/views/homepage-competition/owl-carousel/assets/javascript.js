02ce59f7736eb0697390fc33d69d49c920f95af3
thduy2003 <duytrieudong@gmail.com>
2022-02-14 22:13:38 +0700

    update

--- resources/views/homepage-competition/owl-carousel/assets/javascript.js
+++ resources/views/homepage-competition/owl-carousel/assets/javascript.js
@@ -1,27 +1,25 @@
 var owl = $('.owl-carousel');
 owl.owlCarousel({
+    nav: true,
+    items:1,
+    loop:true,
+    margin:0,
+    autoplay:false,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    autoplaySpeed: 2000,
+    navSpeed: 2000,
+    navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
    responsive : {
     0 : {
-        items:1,
         stagePadding: 0,
         margin: 30,
+        dotsEach: false
     },
-    600: {
-        items:1,
-        stagePadding: 0,
+    600:{
         margin: 50,
     },
     1024: {
-        nav: true,
-        items:1,
-        loop:true,
-        margin:0,
-        autoplay:false,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true,
-        autoplaySpeed: 2000,
-        navSpeed: 2000,
-        navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
        stagePadding: 159
     }
 }
