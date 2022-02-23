e015159075b54b6fa0228ad4c111edfacfb4868d
HoaNguyenPhu <hoanguyen2002.nph@gmail.com>
2022-02-23 10:04:29 +0700

    done navigation bar

--- public/js/app.js
+++ public/js/app.js
@@ -57,18 +57,84 @@ $('#menu-mobile').click(function () {
 
 $('.icon-close').click(function () {
   $('.side-bar').toggleClass("show");
-});
+}); //Xử lý show ra danh sách con
+
 $('.intro-btn').click(function () {
   $('nav ul .intro-show').toggleClass("show1");
+  $('.icon-close').addClass('invisible');
+  $('.side-bar__menu').addClass('invisible');
+  $('.intro-show').addClass('visible');
 });
 $('.tech-btn').click(function () {
   $('nav ul .tech-show').toggleClass("show2");
+  $('.icon-close').addClass('invisible');
+  $('.side-bar__menu').addClass('invisible');
+  $('.tech-show').addClass('visible');
 });
 $('.competition-btn').click(function () {
+  $('nav ul .divider-4').toggleClass("toggle-color");
   $('nav ul .competition-show').toggleClass("show3");
+  $('.icon-close').addClass('invisible');
+  $('.side-bar__menu').addClass('invisible');
+  $('.competition-show').addClass('visible');
 });
 $('nav ul li').click(function () {
   $(this).addClass("active").siblings().removeClass("active");
+}); //Xử lý bấm nút quay lại menu
+
+$('#previous-1').click(function () {
+  $('nav ul .intro-show').toggleClass("show1");
+  $('.icon-close').removeClass('invisible');
+  $('.side-bar__menu').removeClass('invisible');
+  $('nav ul .divider-2').toggleClass("toggle-color");
+});
+$('#previous-2').click(function () {
+  $('nav ul .tech-show').toggleClass("show2");
+  $('.icon-close').removeClass('invisible');
+  $('.side-bar__menu').removeClass('invisible');
+  $('nav ul .divider-3').toggleClass("toggle-color");
+});
+$('#previous-3').click(function () {
+  $('nav ul .competition-show').toggleClass("show3");
+  $('.icon-close').removeClass('invisible');
+  $('.side-bar__menu').removeClass('invisible');
+  $('nav ul .divider-4').toggleClass("toggle-color");
+}); //Xử lý đổi màu cho divider
+
+$('.homepage-btn').click(function () {
+  $('nav ul .divider-1').toggleClass("toggle-color");
+  $('nav ul .divider-2').removeClass("toggle-color");
+  $('nav ul .divider-3').removeClass("toggle-color");
+  $('nav ul .divider-4').removeClass("toggle-color");
+  $('nav ul .divider-5').removeClass("toggle-color");
+});
+$('.intro-btn').click(function () {
+  $('nav ul .divider-2').toggleClass("toggle-color");
+  $('nav ul .divider-1').removeClass("toggle-color");
+  $('nav ul .divider-3').removeClass("toggle-color");
+  $('nav ul .divider-4').removeClass("toggle-color");
+  $('nav ul .divider-5').removeClass("toggle-color");
+});
+$('.tech-btn').click(function () {
+  $('nav ul .divider-3').toggleClass("toggle-color");
+  $('nav ul .divider-1').removeClass("toggle-color");
+  $('nav ul .divider-2').removeClass("toggle-color");
+  $('nav ul .divider-4').removeClass("toggle-color");
+  $('nav ul .divider-5').removeClass("toggle-color");
+});
+$('.competition-btn').click(function () {
+  $('nav ul .divider-4').toggleClass("toggle-color");
+  $('nav ul .divider-1').removeClass("toggle-color");
+  $('nav ul .divider-2').removeClass("toggle-color");
+  $('nav ul .divider-3').removeClass("toggle-color");
+  $('nav ul .divider-5').removeClass("toggle-color");
+});
+$('.CTV-btn').click(function () {
+  $('nav ul .divider-5').toggleClass("toggle-color");
+  $('nav ul .divider-1').removeClass("toggle-color");
+  $('nav ul .divider-2').removeClass("toggle-color");
+  $('nav ul .divider-3').removeClass("toggle-color");
+  $('nav ul .divider-4').removeClass("toggle-color");
 });
 
 /***/ }),
--- resources/views/homepage/NavBar.blade.php
+++ resources/views/homepage/NavBar.blade.php
@@ -1,93 +1,99 @@
 <div class="menu-area">
-<div class="cover-nav-destop">
-    <nav class="nav-destop row p-0">
-        <div class="logo col-4">
-            <img class="logo_img" src="img/Logo ET.png" alt="Logo ET">
-            <span class="logo_text">Economic Technology</span>
-        </div>
-        <div class="nav col-8">
-            <a class="nav-link active"> <span>Trang chủ</span></a>
-            <div class="nav-link nav-link__introduce">
-                <span>Giới thiệu <i class="fas fa-angle-down"></i></span>
-                
-                <div class="more">
-                    <div class="more__border"></div>
-                    <li><a href="#">Về CLB</a></li>
-                    <hr>
-                    <li><a href="#">Cơ cấu nhân sự</a></li>
-                </div>
+    <div class="cover-nav-destop">
+        <nav class="nav-destop nav-container row p-0">
+            <div class="logo col-4">
+                <img class="logo_img" src="img/Logo ET.png" alt="Logo ET">
+                <a href="#" class="logo_text">Economic Technology</a>
             </div>
-            <div class="nav-link nav-link__grTechnology">
-                <span>Góc công nghệ <i class="fas fa-angle-down"></i></span>
-                
-                <div class="more">
-                    <div class="more__border"></div>
-                    <li><a href="#">Bản tin ET</a></li>
-                    <hr>
-                    <li><a href="#">Chuỗi hoạt động hướng dẫn nâng cao năng lực công nghệ cho SV</a></li>
-                    <hr>
-                    <li><a href="#">Các cuộc thi về công nghệ</a></li>
+            <div class="nav col-8">
+                <a class="nav-link active"> <span>Trang chủ</span></a>
+                <div class="nav-link nav-link__introduce">
+                    <span>Giới thiệu <i class="fas fa-angle-down"></i></span>
+                    
+                    <div class="more">
+                        <div class="more__border"></div>
+                        <li><a href="#">Về CLB</a></li>
+                        <hr>
+                        <li><a href="#">Cơ cấu nhân sự</a></li>
+                    </div>
                 </div>
-            </div>
-            <div class="nav-link nav-link__contest">
-                <span>Cuộc thi <i class="fas fa-angle-down"></i></span>
-                
-                <div class="more">
-                    <div class="more__border"></div>
-                    <li><a href="#">Techconomy</a></li>
-                    <hr>
-                    <li><a href="#">Tất cả các cuộc thi</a></li>
+                <div class="nav-link nav-link__grTechnology">
+                    <span>Góc công nghệ <i class="fas fa-angle-down"></i></span>
+                    
+                    <div class="more">
+                        <div class="more__border"></div>
+                        <li><a href="#">Bản tin ET</a></li>
+                        <hr>
+                        <li><a href="#">Chuỗi hoạt động hướng dẫn nâng cao năng lực công nghệ cho SV</a></li>
+                        <hr>
+                        <li><a href="#">Các cuộc thi về công nghệ</a></li>
+                    </div>
                 </div>
+                <div class="nav-link nav-link__contest">
+                    <span>Cuộc thi <i class="fas fa-angle-down"></i></span>
+                    
+                    <div class="more">
+                        <div class="more__border"></div>
+                        <li><a href="#">Techconomy</a></li>
+                        <hr>
+                        <li><a href="#">Tất cả các cuộc thi</a></li>
+                    </div>
+                </div>
+                <a class="nav-link"><span>Tuyển CTV</span></a>
             </div>
-            <a class="nav-link"><span>Tuyển CTV</span></a>
-        </div>
 
-    </nav>
-</div>
+        </nav>
+    </div>
 
-<div class="cover-nav-mobile">
+    <div class="cover-nav-mobile">
         <div class="nav-mobile">
             <div>
                 <img src="img/Logo ET.png" alt="Logo ET">
-                <span>Economic Technology</span>
+                <a href="#">Economic Technology</a>
             </div>
             <i id ="menu-mobile" class="fas fa-bars"></i>
         </div>
 
         <nav class="side-bar">
             <i class="far fa-times icon-close"></i>
-            <ul>
-                <li class="active"><a href="#">Trang chủ</a></li>
-                <li>
-                   <a href="#" class="intro-btn">Giới thiệu
-                   <span class="fas fa-caret-down first"></span>
-                   </a>
-                   <ul class="intro-show more">
-                      <li><a href="#">Về CLB</a></li>
-                      <li><a href="#">Cơ cấu tổ chức</a></li>
-                   </ul>
+            <ul class="side-bar__menu">
+                <li class="active">
+                    <a href="#" class="homepage-btn">Trang chủ</a>
+                    <hr class="divider-1">
                 </li>
                 <li>
-                   <a href="#" class="tech-btn">Góc công nghệ
-                   <span class="fas fa-caret-down second"></span>
-                   </a>
-                   <ul class="tech-show more">
-                      <li><a href="#">Bản tin ET</a></li>
-                      <li><a href="#">Chuỗi hoạt động hướng dẫn nâng cao năng lực công nghệ cho sinh viên</a></li>
-                      <li><a href="#">Các cuộc thi về công nghệ</a></li>
+                    <a href="#" class="intro-btn">Giới thiệu<i id="next" class="fas fa-angle-down"></i></a>
+                    <hr class="divider-2">
+                    <ul class="intro-show">
+                        <i id="previous-1" class="fas fa-angle-down"></i>
+                        <li><a href="#">Về CLB</a><hr></li>
+                        <li><a href="#">Cơ cấu tổ chức</a><hr></li>
                     </ul>
                 </li>
                 <li>
-                    <a href="#" class="competition-btn">Cuộc thi
-                    <span class="fas fa-caret-down second"></span>
-                    </a>
-                    <ul class="competition-show more">
-                       <li><a href="#">Techconomy</a></li>
-                       <li><a href="#">Tất cả các cuộc thi</a></li>
+                    <a href="#" class="tech-btn">Góc công nghệ<i id="next" class="fas fa-angle-down"></i></a>
+                    <hr class="divider-3">
+                    <ul class="tech-show">
+                        <i id="previous-2" class="fas fa-angle-down"></i>
+                        <li><a href="#">Bản tin ET</a><hr></li>
+                        <li><a href="#">Chuỗi hoạt động hướng dẫn nâng cao năng lực công nghệ cho sinh viên</a><hr></li>
+                        <li><a href="#">Các cuộc thi về công nghệ</a><hr></li>
                     </ul>
-                 </li>
-                <li><a href="#">Tuyển CTV</a></li>
-             </ul>
-         </nav>
-</div>
+                </li>
+                <li>
+                    <a href="#" class="competition-btn">Cuộc thi<i id="next" class="fas fa-angle-down"></i></a>
+                    <hr class="divider-4">
+                    <ul class="competition-show">
+                        <i id="previous-3" class="fas fa-angle-down"></i>
+                        <li><a href="#">Techconomy</a><hr></li>
+                        <li><a href="#">Tất cả các cuộc thi</a><hr></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" class="CTV-btn">Tuyển CTV</a>
+                    <hr class="divider-5">
+                </li>
+            </ul>
+        </nav>
+    </div>
 </div>
