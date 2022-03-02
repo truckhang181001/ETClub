7584825d58cab1b0d4b4faea10565cff61c9fa29
KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>
2022-03-02 22:27:15 +0700

    add file info contest upper

---
+++ resources/views/InfoContestPage/InfoContestUpper.blade.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <h1>CUỘC THI ECOMMERCE <br> EXPLORING IN THE TIME OF COVID-19</h1>
+
+    <div class="timeline row">
+        <div class="timeline__round timeline__round--top col">
+            <div class="line"></div>
+            <img src="/img/InfoContestPage/Group 1891.png" alt="">
+        </div>
+        <div class="timeline__round timeline__round--bottom col">
+            <div class="line"></div>
+            <img src="/img/InfoContestPage/Group 1891.png" alt="">
+        </div>
+        <div class="timeline__round timeline__round--top col">
+            <div class="line"></div>
+            <img src="/img/InfoContestPage/Group 1891.png" alt="">
+        </div>
+        
+    </div>
+</div>
--- resources/views/InfoContestPage/index.blade.php
+++ resources/views/InfoContestPage/index.blade.php
@@ -1,9 +1,9 @@
-Alo alo alo
+
 @extends('layout')
 
 @section('title','Cuộc thi')
 
 @section('content')
-    //nhớ include các file .blade.php ở đây
+    @include('InfoConTestPage.InfoContestUpper')
 
 @endsection
