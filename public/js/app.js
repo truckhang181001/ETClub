6bfb6e89432dfca1959b7280bb17dea6d29dc807
keon11011 <leminhquan11011@gmail.com>
2022-03-03 21:30:17 +0700

    Bổ sung nút drop down thông tin đội thi

--- public/js/app.js
+++ public/js/app.js
@@ -33,7 +33,7 @@ $('.owl-carousel-banner').owlCarousel({
 /***/ (() => {
 
 //Xử lý khi bấm icon drop-down
-$('.arrow-1').click(function () {
+$('.name-1, .avatar-1').click(function () {
   $('.arrow-1').toggleClass("rotate");
   $('.name-1').toggleClass("z-index");
   $('.avatar-1').toggleClass("z-index");
@@ -41,6 +41,47 @@ $('.arrow-1').click(function () {
   $('.gold').toggleClass("blur");
   $('.copper').toggleClass("blur");
   $('.center').toggleClass("blur");
+});
+$('.name-2, .avatar-2').click(function () {
+  $('.arrow-2').toggleClass("rotate");
+  $('.name-2').toggleClass("z-index");
+  $('.avatar-2').toggleClass("z-index");
+  $('.name-info-2').toggleClass("display");
+  $('.silver').toggleClass("blur");
+  $('.copper').toggleClass("blur");
+  $('.center').toggleClass("blur");
+});
+$('.name-3, .avatar-3').click(function () {
+  $('.arrow-3').toggleClass("rotate");
+  $('.name-3').toggleClass("z-index");
+  $('.avatar-3').toggleClass("z-index");
+  $('.name-info-3').toggleClass("display");
+  $('.silver').toggleClass("blur");
+  $('.gold').toggleClass("blur");
+  $('.center').toggleClass("blur");
+});
+$('.name-4, .avatar-4').click(function () {
+  $('.arrow-4').toggleClass("rotate");
+  $('.name-4').toggleClass("z-index");
+  $('.avatar-4').toggleClass("z-index");
+  $('.name-info-4').toggleClass("display");
+  $('.consolation-2').toggleClass("blur");
+  $('.beloved').toggleClass("blur");
+});
+$('.name-5, .avatar-5').click(function () {
+  $('.arrow-5').toggleClass("rotate");
+  $('.name-5').toggleClass("z-index");
+  $('.avatar-5').toggleClass("z-index");
+  $('.name-info-5').toggleClass("display");
+  $('.consolation-1').toggleClass("blur");
+  $('.beloved').toggleClass("blur");
+});
+$('.name-6, .avatar-6').click(function () {
+  $('.arrow-6').toggleClass("rotate");
+  $('.name-6').toggleClass("z-index");
+  $('.avatar-6').toggleClass("z-index");
+  $('.name-info-6').toggleClass("display");
+  $('.sponsor-blur').toggleClass("blur");
 });
 
 /***/ }),
--- resources/views/InfoContestPage/info-contest--result__team.blade.php
+++ resources/views/InfoContestPage/info-contest--result__team.blade.php
@@ -2,7 +2,7 @@
 <div class="container-fluid ">
     <div class="row">
         <div class="col-4 silver upper text-center">
-            <img class="avatar-1" src="/img/InfoContestPage/Group 1916.png" alt="">
+            <img class="avatar avatar-1" src="/img/InfoContestPage/Group 1916.png" alt="">
             <div>
                 <h2 class="name name-1">Tên đội thi
                     <i class="fas fa-caret-down arrow-1"></i>
@@ -36,97 +36,218 @@
             </div>
         </div>
         <div class="col-4 gold upper text-center">
-                <img class="avatar" src="/img/InfoContestPage/Group 1916.png" alt="">
-                <div>
-                    <h2>Tên đội thi</h2>
-                    <i class="fa-solid fa-play"></i>
-                </div>
+            <img class="avatar avatar-2" src="/img/InfoContestPage/Group 1916.png" alt="">
+            <div>
+                <h2 class="name name-2">Tên đội thi
+                    <i class="fas fa-caret-down arrow-2"></i>
+                    <div class="name-info name-info-2">
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                    </div>
+                </h2>
+            </div>
         </div>
         <div class="col-4 copper upper text-center">
-                <img class="avatar" src="/img/InfoContestPage/Group 1916.png" alt="">
-                <div>
-                    <h2>Nguyễn Văn A</h2>
-                    <div>
-                        <div class="majors">Hệ thống thông tin</div>
-                        <span class="hyphen"> - </span>
-                        <div class="year-school">K45 - UEH</div>
+            <img class="avatar avatar-3" src="/img/InfoContestPage/Group 1916.png" alt="">
+            <div>
+                <h2 class="name name-3">Tên đội thi
+                    <i class="fas fa-caret-down arrow-3"></i>
+                    <div class="name-info name-info-3">
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </h2>
+            </div>
         </div>
     </div>
     <div class="row">
         <img class="center" src="/img/InfoContestPage/Trophie.png" alt=""> 
     </div>
-    <div class="row justify-content-around">
-        <div class="col-6 below text-center">
+    <div class="row onsolationjustify-content-around">
+        <div class="col-6 consolation-1 below text-center">
             <div class="h2-style--yellow">KHUYẾN KHÍCH</div>
-            <img class="avatar" src="/img/InfoContestPage/Group 1916.png" alt="">
+            <img class="avatar avatar-4" src="/img/InfoContestPage/Group 1916.png" alt="">
             <div>
-                <h2>Nguyễn Văn A</h2>
-                <div>
-                    <div class="majors">Hệ thống thông tin</div>
-                    <span class="hyphen"> - </span>
-                    <div class="year-school">K45 - UEH</div>
-                </div>
+                <h2 class="name name-4">Tên đội thi
+                    <i class="fas fa-caret-down arrow-4"></i>
+                    <div class="name-info name-info-4">
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                    </div>
+                </h2>
             </div>
         </div>
-        <div class="col-6 below text-center">
+        <div class="col-6 consolation-2 below text-center">
             <div class="h2-style--yellow">KHUYẾN KHÍCH</div>
-            <img class="avatar" src="/img/InfoContestPage/Group 1916.png" alt="">
+            <img class="avatar avatar-5" src="/img/InfoContestPage/Group 1916.png" alt="">
             <div>
-                <h2>Nguyễn Văn A</h2>
-                <div>
-                    <div class="majors">Hệ thống thông tin</div>
-                    <span class="hyphen"> - </span>
-                    <div class="year-school">K45 - UEH</div>
-                </div>
+                <h2 class="name name-5">Tên đội thi
+                    <i class="fas fa-caret-down arrow-5"></i>
+                    <div class="name-info name-info-5">
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                    </div>
+                </h2>
             </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-12 text-center">
+        <div class="col-12 beloved text-center">
             <div class="h2-style--yellow">THÍ SINH ĐƯỢC YÊU THÍCH NHẤT</div>
-            <img class="avatar" src="/img/InfoContestPage/Group 1916.png" alt="">
+            <img class="avatar avatar-6" src="/img/InfoContestPage/Group 1916.png" alt="">
             <div>
-                <h2>Nguyễn Văn A</h2>
-                <div>
-                    <div class="majors">Hệ thống thông tin</div>
-                    <span class="hyphen"> - </span>
-                    <div class="year-school">K45 - UEH</div>
-                </div>
+                <h2 class="name name-6">Tên đội thi
+                    <i class="fas fa-caret-down arrow-6"></i>
+                    <div class="name-info name-info-6">
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                        <div class="name-info_detail d-flex">
+                            <div class="member-name">Nguyễn Văn A</div>
+                            <div class="d-flex info">
+                                <div class="member-majors">Hệ thống thông tin</div>
+                                <span class="member-hyphen"> - </span>
+                                <div class="member-year-school">K46 - UEH</div>
+                            </div>
+                        </div>
+                    </div>
+                </h2>
             </div>
         </div>
     </div>
-    <div class="row text-center">
-        <h1>NHÀ TÀI TRỢ</h1>
-    </div>
-    <div class="row text-center">
-        <h2>NHÀ TÀI TRỢ KIM CƯƠNG</h2>
-    </div>
-    <div class="row text-center">
-        <div class="col-12 text-center">
-            <a href="#">
-                <img class="partners" src="/img/InfoContestPage/shanggri-la.png" alt="">
-            </a> 
-            <a href="#">
-                <img class="partners" src="/img/InfoContestPage/infinity-blockchain-venture.png" alt="">
-            </a> 
+    <div class="sponsor-blur">
+        <div class="row text-center">
+            <h1>NHÀ TÀI TRỢ</h1>
         </div>
-    </div>
-    <div class="row text-center">
-        <h2>NHÀ TÀI TRỢ VÀNG</h2>
-    </div>
-    <div class="row text-center">
-        <div class="col-12 text-center">
-            <a href="#">
-                <img class="partners" src="/img/InfoContestPage/cam-house.png" alt="">
-            </a> 
-            <a href="#">
-                <img class="partners" src="/img/InfoContestPage/asia-blockchain-reiveiw.png" alt="">
-            </a> 
-            <a href="#">
-                <img class="partners" src="/img/InfoContestPage/diginet.png" alt="">
-            </a> 
+        <div class="row text-center">
+            <h2>NHÀ TÀI TRỢ KIM CƯƠNG</h2>
+        </div>
+        <div class="row text-center">
+            <div class="col-12 text-center">
+                <a href="#">
+                    <img class="partners" src="/img/InfoContestPage/shanggri-la.png" alt="">
+                </a> 
+                <a href="#">
+                    <img class="partners" src="/img/InfoContestPage/infinity-blockchain-venture.png" alt="">
+                </a> 
+            </div>
+        </div>
+        <div class="row text-center">
+            <h2>NHÀ TÀI TRỢ VÀNG</h2>
+        </div>
+        <div class="row text-center">
+            <div class="col-12 text-center">
+                <a href="#">
+                    <img class="partners" src="/img/InfoContestPage/cam-house.png" alt="">
+                </a> 
+                <a href="#">
+                    <img class="partners" src="/img/InfoContestPage/asia-blockchain-reiveiw.png" alt="">
+                </a> 
+                <a href="#">
+                    <img class="partners" src="/img/InfoContestPage/diginet.png" alt="">
+                </a> 
+            </div>
         </div>
     </div>
 
