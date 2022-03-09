c77fb7a2fbd461f3775bda4213b2cde1279f6066
KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>
2022-03-09 16:22:21 +0700

    create branch string-video-page

---
+++ resources/views/StringVideoPage/index.blade.php
@@ -0,0 +1,9 @@
+
+@extends('layout')
+
+@section('title','Chuỗi video')
+
+@section('content')
+    {{-- @include('HomePage.Banner') --}}
+    Nhớ include
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\User\HomePageController;
+use App\Http\Controllers\User\StringVideoPageController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[HomePageController::class,'index']);
+Route::get('/chuoi-video',[StringVideoPageController::class, 'index']);
