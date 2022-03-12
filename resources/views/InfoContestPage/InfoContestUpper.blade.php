83c173bf54b44f27f2140607c7bb110ef1a27043
KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>
2022-03-12 15:41:02 +0700

    check

--- resources/views/InfoContestPage/InfoContestUpper.blade.php
+++ resources/views/InfoContestPage/InfoContestUpper.blade.php
@@ -1,19 +1,4 @@
 <div class="container">
     <h1>CUỘC THI ECOMMERCE <br> EXPLORING IN THE TIME OF COVID</h1>
 
-    <div class="timeline row">
-        <div class="timeline__round timeline__round--top col">
-            <div class="line"></div>
-            <img src="/img/InfoContestPage/Group 1891.png" alt="">
-        </div>
-        <div class="timeline__round timeline__round--bottom col">
-            <div class="line"></div>
-            <img src="/img/InfoContestPage/Group 1891.png" alt="">
-        </div>
-        <div class="timeline__round timeline__round--top col">
-            <div class="line"></div>
-            <img src="/img/InfoContestPage/Group 1891.png" alt="">
-        </div>
-        
-    </div>
 </div>
