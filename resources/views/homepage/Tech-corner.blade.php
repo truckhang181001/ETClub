e9db2aede263f3d54f980b69c7e81de895f23bce
unknown <hoangducluat123@gmail.com>
2022-03-16 17:36:22 +0700

    fix for testing

--- resources/views/homepage/Tech-corner.blade.php
+++ resources/views/homepage/Tech-corner.blade.php
@@ -1,4 +1,4 @@
-    <div class="techconner container">
+    <div class="techconner tech-container">
         <div class="techconner-name">GÓC CÔNG NGHỆ</div>
         <div class="techconner-list">
             <div class="techconner-item">
@@ -23,7 +23,7 @@
                 <div class="techconner-photo">
                     <img src="/img/Rectangle 2556.png" alt="">
                 </div>
-                <div class="techconner-title">Chuỗi hoạt động nâng cao năng lực công nghệ</div>
+                <div class="techconner-title">Chuỗi hoạt động nâng cao năng lực công nghệ cho SV</div>
                 <div class="techconner-desc">Học cùng ET qua từng video
                 </div>
                 <a href="#" class="techconner-btn">Xem chi tiết</a>
@@ -34,10 +34,10 @@
                     <div class="ellipse2 ellipse"></div>
                     <div class="ellipse3 ellipse"></div>
                     <div class="ellipse4 ellipse"></div>
-                    <img src="/img/Rectangle 2555.png" alt="">
+                    <img src="/img/Rectangle 2558.png" alt="">
                 </div>
-                <div class="techconner-title">Các cuộc thi về công nghệ</div>
-                <div class="techconner-desc"> Nơi tỏa sáng tri thức những tài năng công nghệ
+                <div class="techconner-title">Tổng hợp các cuộc thi công nghệ</div>
+                <div class="techconner-desc"> Nơi tỏa sáng tri thức của những tài năng công nghệ
                 </div>
                 <a href="#" class="techconner-btn">Xem chi tiết</a>
             </div>
