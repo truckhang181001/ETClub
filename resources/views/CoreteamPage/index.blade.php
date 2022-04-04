ac2715ae6d5d1053eebbcb8d647d60aa354c7c57
KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>
2022-04-04 12:18:45 +0700

    kha create branch coreteam-page

---
+++ resources/views/CoreteamPage/index.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layout')
+
+@section('title','Ban chủ nhiệm')
+
+@section('content')
+    @include('CoreteamPage.Coreteam')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\User\CoreteamPageController;
 use App\Http\Controllers\User\HomePageController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[HomePageController::class,'index']);
+Route::get('/ban-dieu-hanh',[CoreteamPageController::class,'index']);
