dbbe4de3798bb29a6b2d1ec71e901c042adbfed9
TrucKhang <truckhang181001@gmail.com>
2022-06-07 22:45:34 +0700

    fix layout.blade

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -4,11 +4,9 @@
 @section('title','Trang chủ')
 {{-- Bat buoc co --}}
 @section('content')
-    @include('HomePage.NavBar')
     @include('HomePage.Banner')
     @include('HomePage.Introduction')
     @include('Homepage.Tech-corner')
     @include('HomePage.Competition')
     @include('HomePage.Thinking')
-    @include('HomePage.Footer')
 @endsection
