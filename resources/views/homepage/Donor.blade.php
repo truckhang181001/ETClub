f00639c1b7f2503174ea55b545405de7e9096e28
KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>
2022-06-22 23:23:26 +0700

    add donor for club

---
+++ resources/views/homepage/Donor.blade.php
@@ -0,0 +1,17 @@
+<div class="donor">
+    <h1 class="donor-title">ĐỐI TÁC ĐỒNG HÀNH</h1>
+    <div class="donor-logo">
+        <img class="donor-logo_1" src="./img/Donor/nashtech.png" alt="logo đối tác đồng hành">
+            <img class="donor-logo_2" src="./img/Donor/edu2review.png" alt="logo đối tác đồng hành">
+            <img class="donor-logo_3" src="./img/Donor/momo.png" alt="logo đối tác đồng hành">
+            <img class="donor-logo_4" src="./img/Donor/coffeeandtea.png" alt="logo đối tác đồng hành">
+            <img class="donor-logo_5" src="./img/Donor/intership.png" alt="logo đối tác đồng hành">
+            <img class="donor-logo_6" src="./img/Donor/camhouse.png" alt="logo đối tác đồng hành">
+            <img class="donor-logo_7" src="./img/Donor/infinity.png" alt="logo đối tác đồng hành">
+            <img class="donor-logo_8" src="./img/Donor/topcv.png" alt="logo đối tác đồng hành">
+            <img class="donor-logo_9" src="./img/Donor/diginet.png" alt="logo đối tác đồng hành">
+            <img class="donor-logo_10" src="./img/Donor/scommunications.png" alt="logo đối tác đồng hành">
+            <img class="donor-logo_11" src="./img/Donor/uehinstitute.png" alt="logo đối tác đồng hành">
+            <img class="donor-logo_12" src="./img/Donor/unknow.png" alt="logo đối tác đồng hành">
+    </div>
+</div>
